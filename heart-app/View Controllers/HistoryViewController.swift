c0635360f623f96c3c58b5f07006a42e07bfa81a
Alexandre Ohara <alexandre.ohara@hotmail.com>
2019-08-04 11:51:19 -0300

    update swift build version

---
+++ heart-app/View Controllers/HistoryViewController.swift
@@ -0,0 +1,11 @@
+//
+//  HistoryViewController.swift
+//  heart-app
+//
+//  Created by Alexandre Ohara on 04/08/19.
+//  Copyright © 2019 Alexandre Ohara. All rights reserved.
+//
+
+import UIKit
+import Hero
+import Charts
