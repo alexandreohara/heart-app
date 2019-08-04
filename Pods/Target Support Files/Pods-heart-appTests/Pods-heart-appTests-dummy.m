37571f2f59b60d00165e30e2f2fc414d855db46b
Alexandre Ohara <alexandre.ohara@hotmail.com>
2019-08-04 11:34:58 -0300

    create card views

---
+++ Pods/Target Support Files/Pods-heart-appTests/Pods-heart-appTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_heart_appTests : NSObject
+@end
+@implementation PodsDummy_Pods_heart_appTests
+@end
---
+++ heart-app/Views/CardView.swift
@@ -0,0 +1,42 @@
+//
+//  CardView.swift
+//  heart-app
+//
+//  Created by Alexandre Ohara on 04/08/19.
+//  Copyright © 2019 Alexandre Ohara. All rights reserved.
+//
+
+import UIKit
+import Hero
+
+class CardView: UIView {
+    let titleLabel = UILabel()
+    let subtitleLabel = UILabel()
+    //let imageView = UIImageView(image: #imageLiteral(resourceName: "Unsplash6"))
+    let visualEffectView = UIVisualEffectView(effect: UIBlurEffect(style: .light))
+    
+    //required init?(coder aDecoder: NSCoder) { fatalError() }
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        clipsToBounds = true
+        
+        titleLabel.font = UIFont.boldSystemFont(ofSize: 28)
+        subtitleLabel.font = UIFont.systemFont(ofSize: 12)
+        //imageView.contentMode = .scaleAspectFill
+        
+        //addSubview(imageView)
+        addSubview(visualEffectView)
+        addSubview(titleLabel)
+        addSubview(subtitleLabel)
+    }
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        //imageView.frame = bounds
+        visualEffectView.frame = CGRect(x: 0, y: 0, width: bounds.width, height: 80)
+        titleLabel.frame = CGRect(x: 20, y: 20, width: bounds.width - 40, height: 30)
+        subtitleLabel.frame = CGRect(x: 20, y: 45, width: bounds.width - 40, height: 30)
+    }
+}
