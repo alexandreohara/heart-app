de339ae7a47569b693b453f595e3706e8044a695
Alexandre Ohara <alexandre.ohara@hotmail.com>
2019-08-05 05:28:41 -0300

    implement medicineCard

--- heart-app/View Controllers/MedicineListViewController.swift
+++ heart-app/View Controllers/MedicineListViewController.swift
@@ -13,6 +13,7 @@ class MedicineListViewController: UIViewController {
     let contentCard = UIView()
     let cardView = CardView()
     let medicineCard = CardView()
+    let medicineDescription = UILabel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,9 +26,11 @@ class MedicineListViewController: UIViewController {
         cardView.subtitleLabel.font = UIFont.systemFont(ofSize: FontSize.SUBTITLE_LABEL_BIG)
         cardView.backgroundColor = UIColor.white
         
-        contentCard.backgroundColor = UIColor.red
+        contentCard.backgroundColor = UIColor.white
         contentCard.clipsToBounds = true
         contentCard.addSubview(cardView)
+        contentCard.addSubview(medicineCard)
+        medicineCard.addSubview(medicineDescription)
         
         view.addSubview(contentCard)
         
@@ -40,6 +43,20 @@ class MedicineListViewController: UIViewController {
         let bounds = view.bounds
         contentCard.frame  = bounds
         cardView.frame = CGRect(x: 0, y: 40, width: bounds.width, height: 80)
+        medicineCard.frame = CGRect(x:20, y: cardView.frame.maxY + 20, width: bounds.width - 40, height: 240)
+        medicineCard.layer.cornerRadius = CardConstants.CORNER_RADIUS
+        medicineCard.backgroundColor = UIColor(red:0.77, green:0.67, blue:0.95, alpha:1.0)
+        medicineCard.titleLabel.text = "Propanolol"
+        medicineCard.subtitleLabel.text = "2 pílulas a cada 8 horas"
+        medicineDescription.frame = CGRect(x:20, y: cardView.visualEffectView.frame.maxY + 4, width: medicineCard.frame.width - 20, height: medicineCard.frame.height - medicineCard.visualEffectView.frame.height)
+        medicineDescription.numberOfLines = 0
+        medicineDescription.text = """
+        Prazo de validade: 24 meses a partir da data de fabricação, impressa na embalagem externa do produto.\n
+        Não utilize o medicamento se o prazo de validade estiver vencido.\n
+        Conservar em temperatura ambiente (entre 15 e 30ºC). Proteger da umidade.
+        """
+        medicineDescription.font = UIFont.systemFont(ofSize: FontSize.SUBTITLE_LABEL_DEFAULT)
+        
     }
     
     @objc func handlePan(gr: PanDirectionGestureRecognizer) {
